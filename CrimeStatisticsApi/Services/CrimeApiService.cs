a84e1e5afbf9375cf1a57a7954ed8774d9d7d6a1
Joshua Morgan <JoshuaReubenMorgan@gmail.com>
2026-02-15 19:49:14 +0000

    Added CrimeApiService

---
+++ CrimeStatisticsApi/Services/CrimeApiService.cs
@@ -0,0 +1,24 @@
+﻿using CrimeStatisticsByLongitudeAndLatitude.Models;
+using System.Text.Json;
+
+namespace CrimeStatisticsByLongitudeAndLatitude.Services
+{
+    public class CrimeApiService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<List<Crime>> GetCrimesByLocationAsync(double latitude, double longitude, string month)
+        {
+            string apiUrl = $"https://data.police.uk/api/crimes-street/all-crime?lat={latitude}&lng={longitude}&date={month}";
+
+            var response = await _httpClient.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            
+            var crimes = JsonSerializer.Deserialize<List<Crime>>(content);
+
+            return crimes ?? new List<Crime>();
+        }
+    }
+}
