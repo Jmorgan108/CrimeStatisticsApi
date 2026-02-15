3a19ab2cab5f3679a84ef1559aaebb4087282522
Joshua Morgan <JoshuaReubenMorgan@gmail.com>
2026-02-15 20:30:07 +0000

    Added the required Writes and Reads for the Data Input.
    Added the services and added calles to them where relevant.

--- CrimeStatisticsApi/Program.cs
+++ CrimeStatisticsApi/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CrimeStatisticsByLongitudeAndLatitude.Services;
+
+var apiService = new CrimeApiService();
+var summaryService = new CrimesSummaryService();
+
+Console.WriteLine("Enter latitude: ");
+var latitudeInput = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter longitute: ");
+var longitudeInput = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter month (YYYY-MM): ");
+var month = Console.ReadLine();
+
+var crimes = await apiService.GetCrimesByLocationAsync(latitudeInput, longitudeInput, month);
+
+Console.WriteLine("--------------------------");
+
+Console.WriteLine($"Total crimes returned: {crimes.Count}");
+
+Console.WriteLine("--------------------------");
+
+var summary = summaryService.SummaryByCategories(crimes);
+
+Console.WriteLine("Crime Summary By Category");
+
+Console.WriteLine("-------------------------");
+
+foreach (var crime in summary)
+{
+    Console.WriteLine($"{crime.Category}: {crime.Count}");
+}
+
