c5ac6acf4848d50927b621a0169abc99ac90f019
Joshua Morgan <JoshuaReubenMorgan@gmail.com>
2026-02-15 20:31:48 +0000

    Updated the Summary service, first to Format the names and then to Group and Order the return from the Apis returning Crimes

---
+++ CrimeStatisticsApi/Services/CrimesSummaryService.cs
@@ -0,0 +1,29 @@
+﻿using CrimeStatisticsApi.Models;
+using CrimeStatisticsByLongitudeAndLatitude.Models;
+
+namespace CrimeStatisticsByLongitudeAndLatitude.Services
+{
+    public class CrimesSummaryService
+    {
+        public List<CrimeSummary> SummaryByCategories(List<Crime> crimes)
+        {
+            return crimes.GroupBy(c => c.Category)
+                         .Select(g => new CrimeSummary
+                         {
+                             Category = FormatCategoryName(g.Key),
+                             Count = g.Count()
+                         })
+                         .OrderByDescending(c => c.Count)
+                         .ToList();
+        }
+
+        private string FormatCategoryName(string category)
+        {
+            if (string.IsNullOrWhiteSpace(category))
+                return "Unknown";
+
+            return string.Join(" ", category.Split('-')
+                .Select(word => char.ToUpper(word[0]) + word.Substring(1)));
+        }
+    }
+}
