2f5702e8ac4c3e4ee38a33416143a0c273318157
Joshua Morgan <JoshuaReubenMorgan@gmail.com>
2026-02-15 20:26:55 +0000

    Changed the Crime Model and Made a new one so that I could actually group each individual Crime

--- CrimeStatisticsApi/Models/Crime.cs
+++ CrimeStatisticsApi/Models/Crime.cs
@@ -3,6 +3,5 @@
     public class Crime
     {
         public string Category { get; set; }
-        public int Count { get; set; }
     }
 }
---
+++ CrimeStatisticsApi/Models/CrimeSummary.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace CrimeStatisticsApi.Models
+{
+    public class CrimeSummary
+    {
+        public string Category { get; set; }
+        public int Count { get; set; }
+    }
+}
